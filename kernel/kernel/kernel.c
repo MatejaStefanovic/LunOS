bf4910440e54812545f23662e82521bf2afe28e1
MatejaStefanovic <matejastefanovic214@gmail.com>
2025-05-21 15:54:13 +0200

    Fixed a bug in make file

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -3,18 +3,14 @@
 #include <kernel/klogging.h>
 #include <kernel/idt_init.h>
 #include <kernel/isr_handler.h>
+#include <stdio.h>
 
-struct {
-    uint16_t limit;
-    uint32_t base;
-} __attribute__((packed)) idt_check;
 
 void kernel_main(void) {
-    
-    terminal_initialize(); // Set up VGA for text output  
     init_gdt(); // Set up GDT and reload segment buffers 
     init_idt(); // Set up IDT
-  
+    terminal_initialize(); // Set up VGA for text output  
+   
     kprintf("%d", 5/0);
     
     //kprintf("hello");
