aa8222e6747f23ae3181307a59f3979ee3f3aca1
MatejaStefanovic <matejastefanovic214@gmail.com>
2025-05-21 18:40:58 +0200

    Fixed register push order to fit System V i386 calling convention

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -10,7 +10,7 @@ void kernel_main(void) {
     init_gdt(); // Set up GDT and reload segment buffers 
     init_idt(); // Set up IDT
     terminal_initialize(); // Set up VGA for text output  
-    
+    kprintf("%d", 1/0); 
     kprintf("I started this gangsta shit?! And this the motherfucking chance I get?\n");
     kprintf("                                   HELLO");
     
