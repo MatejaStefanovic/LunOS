91c523798166d90034c95f2b3ff0edfb43162bf0
MatejaStefanovic <matejastefanovic214@gmail.com>
2025-05-22 22:15:26 +0200

    Refactored paging initialization and placed the kernel in higher half

--- kernel/include/kernel/paging.h
+++ kernel/include/kernel/paging.h
@@ -1,41 +1,5 @@
 #ifndef __KERNEL_PAGING_H
 #define __KERNEL_PAGING_H
 
-#include <stdint.h>
 
-struct pde_t {
-    uint8_t present              : 1;  // Bit 0 is page in physical memory?
-    uint8_t r_w                  : 1;  // Bit 1 if it is set page is read+write otherwise read only
-    uint8_t u_s                  : 1;  // Bit 2 sets privilege level - if set accessed by all otherwise read only
-    uint8_t pwt                  : 1;  // Bit 3 page write through caching enable/disable
-    uint8_t pcd                  : 1;  // Bit 4 page cache disabled if set otherwise it is cached
-    uint8_t accessed             : 1;  // Bit 5 was PDE read during VAddr translation
-    uint8_t reserved             : 1;  // Bit 6 (reserved for 4KB pages)
-    uint8_t ps                   : 1;  // Bit 7 is 0 for 4KiB pages, 1 for 4MiB pages (we use 0)
-    uint8_t avl_for_os           : 4;  // Bits 8-11, OS can store info on these bits
-    uint32_t pt_addr             : 20; // Bits 12-31 (address of page table >> 12)
-} __attribute__((packed));
-
-struct pte_t{
-    uint8_t present              : 1;  // Bit 0 is page in physical memory?
-    uint8_t r_w                  : 1;  // Bit 1 if it is set page is read+write otherwise read only
-    uint8_t u_s                  : 1;  // Bit 2 sets privilege level - if set accessed by all otherwise read only
-    uint8_t pwt                  : 1;  // Bit 3 page write through caching enable/disable
-    uint8_t pcd                  : 1;  // Bit 4 page cache disabled if set otherwise it is cached
-    uint8_t accessed             : 1;  // Bit 5 was PDE read during VAddr translation
-    uint8_t dirty                : 1;  // Bit 6 was PDE read during VAddr translation
-    uint8_t pat                  : 1;  // Bit 7 was PDE read during VAddr translation
-    uint8_t global               : 1;  // Bit 8 was PDE read during VAddr translation
-    uint8_t avl_for_os           : 3;  // Bits 9-11, OS can store info on these bits
-    uint32_t pf_addr             : 20; // Bits 12-31 4KiB page frame address  
-} __attribute__((packed));
-
-extern void loadPageDirectory(uint32_t *);
-extern void enablePaging();
-void fill_pd();
-void fill_pt();
-void init_paging();
-
-extern void higher_half_main(void);
-void jump_to_higher_half();
 #endif
--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -8,14 +8,11 @@
 void kernel_main(void) {
     init_gdt(); // Set up GDT and reload segment buffers 
     init_idt(); // Set up IDT
-    init_paging();
     terminal_initialize(); // Set up VGA for text output  
     kprintf("I started this gangsta shit?! And this the motherfucking chance I get?\n");
-    kprintf("                                   HELLO");
-    
-}
-
-void higher_half_main(){
-    kprintf("What happened in Monte Carlo happened, what happened in Barcelona happened ");
-    kprintf("What happened in Madrdid happened and we are here, we're in Rome...");
+    kprintf("                                   HELLO\n");
+    kprintf("What happened in Monte Carlo happened...\n");
+    kprintf("What happened in Barcelona happened...\n");
+    kprintf("What happened in Madrdid happened and we are here, ");
+    kprintf("we're in Rome...");
 }
