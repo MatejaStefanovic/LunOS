239c0dd0567caba59ac2581ef8fc51335ddafc24
MatejaStefanovic <matejastefanovic214@gmail.com>
2025-05-22 20:32:45 +0200

    Enabled paging

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -3,15 +3,19 @@
 #include <kernel/klogging.h>
 #include <kernel/idt_init.h>
 #include <kernel/isr_handler.h>
-#include <stdio.h>
-
+#include <kernel/paging.h>
 
 void kernel_main(void) {
     init_gdt(); // Set up GDT and reload segment buffers 
     init_idt(); // Set up IDT
+    init_paging();
     terminal_initialize(); // Set up VGA for text output  
-    kprintf("%d", 1/0); 
     kprintf("I started this gangsta shit?! And this the motherfucking chance I get?\n");
     kprintf("                                   HELLO");
     
+}
+
+void higher_half_main(){
+    kprintf("What happened in Monte Carlo happened, what happened in Barcelona happened ");
+    kprintf("What happened in Madrdid happened and we are here, we're in Rome...");
 }
